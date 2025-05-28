841e9a02ec71c0ca4e33b096bf73a77224bade87
achiever667 <kelechioleh@gmail.com>
2025-05-27 23:54:34 -0700

    cores file add with vue default api integrated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'; // Set your backend API base URL here
 
 const app = createApp(App);
 app.use(router);
